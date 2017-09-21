ae039742b063aa62b7ef8ddcadbf38577a17b295
Adam Patridge <patridge@gmail.com>
2017-09-21 11:12:03 -0600

    Add files missed by migration.

---
+++ Homework/Hangman/HangmanManager.cs
@@ -0,0 +1,49 @@
+﻿namespace Hangman
+{
+    public class HangmanManager
+    {
+        private string hiddenString;
+
+        public string DisplayString
+        {
+            get; private set;
+        }
+
+        public bool IsGameOver
+        {
+            get
+            {
+                return DisplayString == hiddenString;
+            }
+        }
+
+        public HangmanManager(string hiddenWord)
+        {
+            hiddenString = hiddenWord;
+            CreateDisplayString();
+        }
+
+        public bool Guess(char guess)
+        {
+            bool foundALetter = false;
+            for (int i = 0; i < hiddenString.Length; i++)
+            {
+                if (hiddenString[i] == guess)
+                {
+                    DisplayString = DisplayString.Remove(i, 1);
+                    DisplayString = DisplayString.Insert(i, guess.ToString());
+                    foundALetter = true;
+                }
+            }
+            return foundALetter;
+        }
+
+        private void CreateDisplayString()
+        {
+            for (int i = 0; i < hiddenString.Length; i++)
+            {
+                DisplayString += "*";
+            }
+        }
+    }
+}
---
+++ Homework/Hangman/HangmanPage.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Hangman
+{
+    public class HangmanPage : ContentPage
+    {
+        private Label hiddenMessageLabel;
+        private Entry guessEntry;
+
+        private HangmanManager hangManager;
+
+        public HangmanPage()
+        {
+            hangManager = new HangmanManager("xamarin");
+
+            hiddenMessageLabel = new Label { FontSize = 36, HorizontalTextAlignment = TextAlignment.Center };
+            guessEntry = new Entry { Placeholder = "Enter a single character" };
+            Button guessButton = new Button { Text = "Guess" };
+
+            guessButton.Clicked += GuessButton_Clicked;
+
+            Content = new StackLayout
+            {
+                Padding = 20,
+                Children = {
+                    hiddenMessageLabel,
+                    guessEntry,
+                    guessButton
+                }
+            };
+            UpdateUI();
+        }
+
+        private void GuessButton_Clicked(object sender, EventArgs e)
+        {
+            char guessedLetter;
+            if (char.TryParse(guessEntry.Text, out guessedLetter))
+            {
+                // Always test lowercase.
+                guessedLetter = Char.ToLower(guessedLetter);
+                if (hangManager.Guess(guessedLetter))
+                {
+                    UpdateUI();
+                }
+                //after we guess lets clear the entry
+                guessEntry.Text = "";
+                //after we guess check to see if we have completed the puzzle
+                if (hangManager.IsGameOver)
+                {
+                    DisplayAlert("You Win!", "Good Job! You have completed the puzzle.", "Okay");
+                }
+            }
+            else
+            {
+                DisplayAlert("Not a Valid Guess", "Please enter a single character", "Okay");
+            }
+        }
+
+        private void UpdateUI()
+        {
+            hiddenMessageLabel.Text = hangManager.DisplayString;
+        }
+    }
+}
